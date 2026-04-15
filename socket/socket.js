64ecd9882004691568cca9fe0c13015f36ac0e0c
Aman <amanshakya415@gmail.com>
2026-04-15 16:55:14 +0530

    bug fixed

--- socket/socket.js
+++ socket/socket.js
@@ -4,15 +4,23 @@ let io;
 
 export const initSocket = (server) => {
   io = new Server(server, {
-    cors: { origin: "*" }
+    cors: {
+      origin: [
+        "http://localhost:5173", // dev frontend
+        "https://pingchatify.netlify.app/" // (optional production)
+      ],
+      credentials: true,
+    },
   });
 
   io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
-    if (userId) socket.join(userId); // User ko unki ID ke room mein join karayein
+
+    if (userId) {
+      socket.join(userId);
+    }
 
     socket.on("sendMessage", (data) => {
-      // Message sirf receiverId wale room ko bhejien
       io.to(data.receiverId).emit("receiveMessage", data);
     });
 
