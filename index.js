d5a9994719de5f612882921b180a18a521666f58
Aman <amanshakya415@gmail.com>
2026-04-15 16:59:58 +0530

    fix

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ dotenv.config()
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://pingchatify.netlify.app/", // your frontend
+  credentials: true,
+}));
 app.use(express.json());
 
 // routes
